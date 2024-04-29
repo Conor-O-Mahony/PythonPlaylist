ee43b11c1c97981476e360d451508932ceef0980
Conor-O-Mahony <conor.omahony@ucdconnect.ie>
2024-04-29 16:17:31 +0100

    Changed Playlist to circular doubly linked list

--- playlist.py
+++ playlist.py
@@ -11,22 +11,30 @@ class Playlist:
         # Insert a new Track at the beginning of the linked list
         # Time Complexity: O(1), Auxiliary Space: O(1)
         new_track = track.Track(name,album,length)
-        new_track.next = self.head
+
+        if not self.head:
+            new_track.next = new_track
+            new_track.prev = new_track
+        else:
+            new_track.next = self.head
+            new_track.prev = self.head.prev
+            self.head.prev.next = new_track
+            self.head.prev = new_track
         self.head = new_track
 
     def insert_at_end(self,name,album,length):
         # Append a new Track at the end of the linked list
-        # Time Complexity: O(N), Auxiliary Space: O(1)
+        # Time Complexity: O(1), Auxiliary Space: O(1)
         new_track = track.Track(name,album,length)
+
         if not self.head:
-            #Linked list empty
-            self.head = new_track
-            return
-        current = self.head
-        while current.next:
-            # Traverse until last node
-            current = current.next
-        current.next = new_track
+            new_track.next = new_track
+            new_track.prev = new_track
+        else:
+            new_track.next = self.head
+            new_track.prev = self.head.prev
+            self.head.prev.next = new_track #new_track.next.prev = new_track #?????
+            self.head.prev = new_track
 
     def insert_at_index(self,pos,name,album,length):
         # Insert a new track at the specified index
@@ -37,23 +45,24 @@ class Playlist:
         if pos == 0:
             self.insert_at_beginning(name,album,length)
             return
+        
         new_track = track.Track(name,album,length)
         current = self.head
         count = 0
-        while count < pos - 1 and current:
+        while count < pos - 1:
             # Traverse until the track before pos
             current = current.next
             count += 1
-        if not current:
-            # If the pos out of range
-            print("Position out of range")
-            return
+
         # Insert the new node at the specified position
         new_track.next = current.next
+        new_track.prev = current
+        current.next.prev = new_track
         current.next = new_track
 
     #Methods for deletion
     def delete_at_beginning(self):
+        #Delete track from beginning of the playlist
         # Time Complexity: O(1), Auxiliary Space: O(1)
         if not self.head:
             print(self.name," is empty!")
@@ -64,20 +73,15 @@ class Playlist:
             self.head = None
             return
 
-        current = self.head
-        while current.next != self.head:
-            current = current.next
-            
-        # Make last track point to second
-        current.next = self.head.next
-        
-        # Point head to second track
+        self.head.prev.next = self.head.next
+        self.head.next.prev = self.head.prev
         self.head = self.head.next
 
-    def delete_at_position(self, pos):
+    def delete_at_index(self, pos):
         #Delete track at the specified pos
+        # Time Complexity: O(N), Auxiliary Space: O(1)
         if not self.head:
-            print("Playlist is empty!")
+            print(self.name,"is empty!")
             return
         if pos < 0:
             print("Invalid position")
@@ -89,38 +93,52 @@ class Playlist:
             return
         current = self.head
         count = 0
-        while count < pos - 1 and current.next != self.head:
+        while count < pos - 1:
+            # Traverse until the track before pos
             current = current.next
             count += 1
-        if count < pos - 1:
-            print("Index out of range")
-            return
+
         current.next = current.next.next
+        current.next.next = current.prev
 
     def delete_at_end(self):
         #Delete track from end of playlist
+        # Time Complexity: O(1), Auxiliary Space: O(1)
         if not self.head:
             print("Playlist is empty!")
             return
-        current = self.head
-        while current.next and current.next != self.head:
-            # Traverse until last node
-            current = current.next
-        #FINISH THIS METHOD
+
+        if self.head.next == self.head:
+            self.head = None
+            return
+
+        self.head.prev.prev.next = self.head
+        self.head.prev = self.head.prev.prev
         
     def contents(self):
         # Display the elements of the linked list
+        # Time Complexity: O(N), Auxiliary Space: O(1)
         current = self.head
         count=0
-        while current:
+        if not self.head:
+            print(self.name,"is empty")
+            return
+        while True:
             print("Track",count,"->",current)
+            print("Next track ->",current.next)
+            print("Previous track ->",current.prev)
+            print()
             current = current.next
             count+=1
+            if current == self.head:
+                return
 
 if __name__ == '__main__':
     playlist = Playlist("test_playlist")
     playlist.insert_at_beginning("artist1","album1",120)
-    playlist.insert_at_end("artist2","album2",100)
-    playlist.insert_at_index(1,"artist3","album3",210)
+    playlist.insert_at_end("artist3","album3",100)
+    playlist.insert_at_index(1,"artist2","album2",210)
     playlist.insert_at_index(3,"artist4","album4",40)
+
+    playlist.delete_at_index(2)
     playlist.contents()
