262aa42f5a41bff71efc77d8cd5e882b4211b708
Conor-O-Mahony <conor.omahony@ucdconnect.ie>
2024-05-05 13:59:13 +0100

    Added complexity notes to track

--- track.py
+++ track.py
@@ -10,11 +10,15 @@ class Track:
         self.prev = None
 
     def compare_attributes(obj1, obj2, attr):
+        #Compare attributes of 2 different Track objects
+        #Time Complexity O(1), Space Complexity O(1)
         if getattr(obj1, attr) < getattr(obj2, attr):
             return True
         return False
         
     def attribute_exists(obj, attr):
+        #Check if Track attribute exists
+        #Time Complexity O(1), Space Complexity O(1)
          try:
               getattr(obj, attr)
               return True
